2e40fc0193d34ff9337b3eed36b9eb765c0d75d0
EC2 Default User <ec2-user@ip-172-31-38-204.ap-south-1.compute.internal>
2023-08-02 04:21:06 +0000

    name change s3

--- resource_stack/resource_stack.py
+++ resource_stack/resource_stack.py
@@ -24,5 +24,5 @@ class ResourceStack(Stack):
                                             handler="demo_lambda.lambda_handler")
 
         bucket = s3.Bucket(self, "MyfirstBucket", versioned=True,
-                           bucket_name="tests3bucketasof26julyyy11latestt",
+                           bucket_name="tests3bucketasof26julyyy11latestt001",
                            block_public_access=s3.BlockPublicAccess.BLOCK_ALL)
